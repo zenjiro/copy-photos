48d2eb4fbba313660b5dad34fa1f9249054cb26b
zenjiro <zenjiro0123@gmail.com>
2024-04-09 22:57:09 +0900

    pylintの渓谷に対応

--- config.sample.py
+++ config.sample.py
@@ -1,6 +1,7 @@
+"""設定ファイルのサンプル"""
 # コピー元のディレクトリー。yyyy_MM_dd/*.JPGの形式で写真が保存されている。
-dcim_dir = "/path/to/dcim/directory"
+DCIM_DIR = "/path/to/dcim/directory"
 # コピー先のディレクトリー。ここにある撮影日時の最も新しい写真より新しい写真がコピーされる。
-pictures_dir = "/path/to/pictures/directory"
+PICTURES_DIR = "/path/to/pictures/directory"
 # コピー先のディレクトリー。ここにも写真がコピーされる。
-pictures_dir2 = "/path/to/pictures/directory2"
+PICTURES_DIR2 = "/path/to/pictures/directory2"
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# お気に入りにマークした写真を壁紙フォルダーにコピー
+"""お気に入りにマークした写真を壁紙フォルダーにコピー """
 import datetime
 import glob
 import os
@@ -7,68 +7,74 @@ import shutil
 
 import pyexiv2
 
-from config import dcim_dir, pictures_dir, pictures_dir2
+from config import DCIM_DIR, PICTURES_DIR, PICTURES_DIR2
 
-if __name__ == "__main__":
-    # XXX 「RuntimeError: Directory Canon with 13312 entries considered invalid; not read.」エラーを抑制
+
+def main():
+    """メイン処理"""
+    # 「RuntimeError: Directory Canon with 13312 entries considered invalid; not read.」エラーを抑制
     pyexiv2.set_log_level(4)
     latest_timestamp = sorted(
         [
-            datetime.datetime.fromtimestamp(os.path.getctime(f"{pictures_dir}/{file}"))
-            for file in glob.iglob("*.JPG", root_dir=pictures_dir)
+            datetime.datetime.fromtimestamp(os.path.getctime(f"{PICTURES_DIR}/{file}"))
+            for file in glob.iglob("*.JPG", root_dir=PICTURES_DIR)
         ]
     )[-1]
     latest_date_time = ""
-    for file in glob.iglob("*.JPG", root_dir=pictures_dir):
+    for file in glob.iglob("*.JPG", root_dir=PICTURES_DIR):
         timestamp = datetime.datetime.fromtimestamp(
-            os.path.getctime(f"{pictures_dir}/{file}")
+            os.path.getctime(f"{PICTURES_DIR}/{file}")
         )
         if latest_timestamp - timestamp < datetime.timedelta(hours=1):
-            with pyexiv2.Image(f"{pictures_dir}/{file}", encoding="CP932") as img:
+            with pyexiv2.Image(f"{PICTURES_DIR}/{file}", encoding="CP932") as img:
                 latest_date_time = max(
                     latest_date_time, img.read_exif().get("Exif.Image.DateTime", latest_date_time)
                 )
     latest_date = re.sub(" .+", "", latest_date_time).replace(":", "_")
-    for file in glob.iglob("*/*.JPG", root_dir=dcim_dir):
+    for file in glob.iglob("*/*.JPG", root_dir=DCIM_DIR):
         dir_name = os.path.dirname(file)
         basename = os.path.basename(file)
         if dir_name >= latest_date:
-            with pyexiv2.Image(f"{dcim_dir}/{file}", encoding="CP932") as img:
+            with pyexiv2.Image(f"{DCIM_DIR}/{file}", encoding="CP932") as img:
                 rating = int(img.read_xmp()["Xmp.xmp.Rating"]) if "Xmp.xmp.Rating" in img.read_xmp() else -1
                 if rating >= 4:
                     date_time = img.read_exif()["Exif.Image.DateTime"]
                     if date_time > latest_date_time:
-                        if os.path.isfile(f"{pictures_dir}/{basename}"):
+                        if os.path.isfile(f"{PICTURES_DIR}/{basename}"):
                             splitext = os.path.splitext(basename)
                             for i in range(2, 100):
                                 target = (
-                                    f"{pictures_dir}/{splitext[0]} ({i}){splitext[1]}"
+                                    f"{PICTURES_DIR}/{splitext[0]} ({i}){splitext[1]}"
                                 )
                                 if not os.path.isfile(target):
-                                    print(f"copying {dcim_dir}/{file} to {target}")
-                                    shutil.copy2(f"{dcim_dir}/{file}", target)
+                                    print(f"copying {DCIM_DIR}/{file} to {target}")
+                                    shutil.copy2(f"{DCIM_DIR}/{file}", target)
                                     break
                         else:
                             print(
-                                f"copying {dcim_dir}/{file} to {pictures_dir}/{basename}"
+                                f"copying {DCIM_DIR}/{file} to {PICTURES_DIR}/{basename}"
                             )
                             shutil.copy2(
-                                f"{dcim_dir}/{file}", f"{pictures_dir}/{basename}"
+                                f"{DCIM_DIR}/{file}", f"{PICTURES_DIR}/{basename}"
                             )
-                        if os.path.isfile(f"{pictures_dir2}/{basename}"):
+                        if os.path.isfile(f"{PICTURES_DIR2}/{basename}"):
                             splitext = os.path.splitext(basename)
                             for i in range(2, 100):
                                 target = (
-                                    f"{pictures_dir2}/{splitext[0]} ({i}){splitext[1]}"
+                                    f"{PICTURES_DIR2}/{splitext[0]} ({i}){splitext[1]}"
                                 )
                                 if not os.path.isfile(target):
-                                    print(f"copying {dcim_dir}/{file} to {target}")
-                                    shutil.copy2(f"{dcim_dir}/{file}", target)
+                                    print(f"copying {DCIM_DIR}/{file} to {target}")
+                                    shutil.copy2(f"{DCIM_DIR}/{file}", target)
                                     break
                         else:
                             print(
-                                f"copying {dcim_dir}/{file} to {pictures_dir2}/{basename}"
+                                f"copying {DCIM_DIR}/{file} to {PICTURES_DIR2}/{basename}"
                             )
                             shutil.copy2(
-                                f"{dcim_dir}/{file}", f"{pictures_dir2}/{basename}"
+                                f"{DCIM_DIR}/{file}", f"{PICTURES_DIR2}/{basename}"
                             )
+
+
+if __name__ == "__main__":
+    main()
